e6a4498f352e799377d6a1e22e36e16e037b67a8
Elijah Masanga <emasanga@aifrruislabs.com>
2024-07-29 18:27:43 +0300

    Enforce SSL on production

--- source_code/app/Providers/AppServiceProvider.php
+++ source_code/app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use View;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+          //Second Way of Forcing SSL Scheme
+          URL::forceScheme('https');
+
+          //Forcing SSL
+          if (env('APP_ENV') == 'production') {
+             $this->app['request']->server->set('HTTPS', true);
+          }
+  
+          //Schema Default String Length
+          Schema::defaultStringLength(191);
+  
+          //Sharing to All Views
+          View::share([
+              'appTitle' => 'Pipe SMS',
+          ]);
+  
     }
 }
